a2abbf831ca357ee52f9e58b8ca5b3a336753d48
Silas Henrique <silas.henrique99@outlook.com>
2021-05-20 00:04:05 -0300

    Aula 07 - Value Object

---
+++ 07 REST_ADID6 Value Object/src/main/java/br/edu/ifsp/repository/PersonRepository.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ifsp.data.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+}
