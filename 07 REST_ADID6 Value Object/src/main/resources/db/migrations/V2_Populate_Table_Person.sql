739daefbf584be9309766edd02f724461b12ca6d
Silas Henrique <silas.henrique99@outlook.com>
2021-06-28 20:30:57 -0300

    10 REST_ADID6 - Hateoas

---
+++ 07 REST_ADID6 Value Object/src/main/resources/db/migrations/V2_Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Rua Monsenhor Jeronimo Rodrigues, 256 - Jardim Climax', 'José', 'Male', 'Henrique'),
+	(2, 'Morumbi', 'Fulano', 'Male', 'Da SIlva');
